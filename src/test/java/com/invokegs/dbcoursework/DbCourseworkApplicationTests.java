8ce6bc99e7acb2b0567c3e4017a17d5fc16b70d0
demkom58 <demkom58@gmail.com>
2021-05-16 07:05:31 +0300

    feat: initial commit

---
+++ src/test/java/com/invokegs/dbcoursework/DbCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invokegs.dbcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
